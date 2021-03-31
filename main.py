905782adf1bb031efe478526d800081403bcf3f3
Konstantinos Drosos <dean.drossos@gmail.com>
2021-03-31 00:42:42 -0400

    Converting starting file to a class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 import tkinter as tk
 from tkinter import StringVar
-
+"""
 # Setup start window
 root = tk.Tk()
 root.geometry('400x400')
@@ -50,5 +50,41 @@ start_game_button.grid(column=0, row=4, pady=100)
 
 cancel_button = tk.Button(root, text='Cancel', font=('Arial', 15), background='grey')
 cancel_button.grid(column=1, row=4, padx=40)
+"""
 
-root.mainloop()
+class Crazy_Monopoly(tk.Frame):
+
+    """Set an class for the application"""
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.create_labels('Crazy Monopoly', 0, 0)
+
+    # Function to create a label giving the text and placement
+    def create_labels(self, label_text, column_loc, row_loc):
+        character_label = tk.Label(self.master, text=label_text, font=('Arial', 15), background='black')
+        character_label.configure(fg='#f5f7f6')
+        character_label.grid(column=column_loc, row=row_loc)
+
+    # Helper function to create input
+    def create_input_text(self, column_loc, row_loc):
+        input_box = tk.Entry()
+        input_box.grid(column=column_loc, row=row_loc)
+
+    # Method to create dropdown box
+    def create_dropdown(self, options, column_loc, row_loc):
+        selection = tk.StringVar()
+        selection.set(options[0])
+        dropdown = tk.OptionMenu(self.master, selection, *options)
+        dropdown.configure(fg='#f5f7f6')
+        dropdown.grid(column=column_loc, row=row_loc)
+
+    # Method to add a button
+    def create_button(self, command, text_but, column_loc, row_loc):
+        button = tk.Button(self.master, text=text_but, font=('Arial', 15), background='grey')
+        button.grid(column=column_loc, row=row_loc)
+
+
+root = tk.Tk()
+crazy_monopoly = Crazy_Monopoly(root)
+crazy_monopoly.mainloop()
