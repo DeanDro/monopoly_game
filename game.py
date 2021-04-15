d43ea296746bd611c35b5e12cc3c49e33248d854
Konstantinos Drosos <dean.drossos@gmail.com>
2021-04-14 23:45:03 -0400

    Completed all cards classes and populate in dictionaries

--- game.py
+++ game.py
@@ -4,6 +4,11 @@ import pygame
 import sys
 import pandas
 from cards_classes.property import Properties
+from cards_classes.special_cards import SpecialCards
+from cards_classes.chance import Chance
+from cards_classes.community import Community
+from cards_classes.energy import Energy
+from cards_classes.transportation import Transportation
 
 
 class Game:
@@ -83,12 +88,17 @@ class CardsData:
         self.cards_dictionary = self.return_dict_card_data()
 
     def return_dict_card_data(self):
-        # Function to get the data loaded from excel to five different dataframes and
-        # then load the data from the dataframes to a dictionary of each card type and then to a major dictionary that
-        # will hold dictionaries from each card type
+        """This is a function to return a dictionary that contains dictionaries with all the different types
+        of cards.
+        """
+        cities_dictionary = self.populate_cities_dictionary()
+        return cities_dictionary
+
+    def populate_cities_dictionary(self):
+        """Helper method to return a dictionary with the data from the dataframe. The method takes as
+        an argument the cards type and a list that contains the names of all columns.
+        """
         cities_dictionary = dict()
-        special_dictionary = dict()
-        transportation_dictionary = dict()
         city_names = self.cities_data['City']
         sell_value = self.cities_data['Sell Value']
         resell_value = self.cities_data['Resell Value']
@@ -99,6 +109,63 @@ class CardsData:
             new_card = Properties(city_names[i], True, None, sell_value[i], rent_value[i],
                                   resell_value[i], house_value[i], hotel_value[i])
             cities_dictionary[city_names[i]] = new_card
-        # Stopping here because realized i need to build special subclasses for the rest of the special cards
         return cities_dictionary
 
+    def populate_special_cards(self):
+        """Method to return a dictionary with special type cards.
+        """
+        special_dictionary = dict()
+        block_name = self.special_data['Block']
+        money_get = self.special_data['Money Get']
+        money_pay = self.special_data['Money Lost']
+        action_card = self.special_data['Action']
+        for i in range(0, len(self.special_data.index)):
+            new_card = SpecialCards(block_name[i], False, money_get[i], money_pay[i], action_card[i])
+            special_dictionary[block_name[i]] = new_card
+        return special_dictionary
+
+    def populate_transportation(self):
+        """Method to return a dictionary with the transportation cards.
+        """
+        transportation_dict = dict()
+        route = self.transportation_data['Route']
+        sell_value = self.transportation_data['Sell Value']
+        resell_value = self.transportation_data['Resell Value']
+        rent = self.transportation_data['Rent']
+        for i in range(0, len(self.transportation_data.index)):
+            new_card = Transportation(route[i], True, sell_value[i], resell_value[i], rent[i])
+            transportation_dict[route[i]] = new_card
+        return transportation_dict
+
+    def populate_energy_dict(self):
+        """Method to return a dictionary with the energy cards.
+        """
+        energy_dict = dict()
+        station = self.energy_data['Station']
+        sell_value = self.energy_data['Sell Value']
+        resell_value = self.energy_data['Resell Value']
+        rent = self.energy_data['Rent']
+        for i in range(0, len(self.energy_data.index)):
+            new_card = Energy(station[i], True, sell_value[i], resell_value[i], rent[i])
+            energy_dict[station[i]] = new_card
+        return energy_dict
+
+    def populate_chance_dict(self):
+        """Method to return a dictionary with all the chance cards.
+        """
+        chance_dict = dict()
+        card_name = self.chance_data['Card']
+        message = self.chance_data['Message']
+        for i in range(0, len(self.chance_data.index)):
+            new_data = Chance(card_name[i], True, message[i])
+            chance_dict[card_name[i]] = new_data
+        return chance_dict
+
+    def populate_community_cards(self):
+        community_dict = dict()
+        card_name = self.community_data['Card']
+        message = self.community_data['Message']
+        for i in range(0, len(self.community_data.index)):
+            new_data = Community(card_name[i], True, message[i])
+            community_dict[card_name[i]] = new_data
+        return community_dict
